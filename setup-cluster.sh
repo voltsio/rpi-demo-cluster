1e4874b87380d674390f8de1f283a917f05b234c
Toby Joe Boudreaux <tj@tobyjoe.com>
2017-12-10 17:19:50 -0500

    i think i need flannel earlier on hypriot

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -34,11 +34,11 @@ ds_wait kube-system kube-proxy 1
 echo -ne " Done\nJoining workers..."
 parallel-ssh -i -h worker-nodes -t 0 "sudo kubeadm join --skip-preflight-checks --token 123456.1234567890123456 10.34.42.182:6443" >> log 2>&1
 
-echo -ne " Done\nWaiting for kube-proxy..."
-ds_wait kube-system kube-proxy 5
-
 echo -ne " Done\nInstalling flannel..."
 kubectl apply -f kube-flannel.yaml >> log 2>&1
+
+echo -ne " Done\nWaiting for kube-proxy..."
+ds_wait kube-system kube-proxy 5
 
 echo -ne " Done\nWaiting for flannel..."
 ds_wait kube-system kube-flannel-ds 5
