e4cd0df68c78294495e8bcf224300303b63a258b
Toby Joe Boudreaux <tj@tobyjoe.com>
2017-12-12 10:39:12 -0500

    ds_wait probs

--- relabel-nodes.sh
+++ relabel-nodes.sh
@@ -2,35 +2,22 @@
 
 set -e
 
-kubectl label node pico0 blinktShow-
-kubectl label node pico1 blinktShow-
-kubectl label node pico2 blinktShow-
-kubectl label node pico3 blinktShow-
-kubectl label node pico4 blinktShow-
+kubectl label node pico0 blinktShow=true --overwrite
+kubectl label node pico0 blinktImage=pods --overwrite
+kubectl label node pico0 blinktReadyColor=cpu --overwrite
 
-kubectl label node pico0 blinktShow=true
-kubectl label node pico1 blinktShow=true
-kubectl label node pico2 blinktShow=true
-kubectl label node pico3 blinktShow=true
-kubectl label node pico4 blinktShow=true
+kubectl label node pico1 blinktShow=true --overwrite
+kubectl label node pico1 blinktImage=pods --overwrite
+kubectl label node pico1 blinktReadyColor=cpu --overwrite
 
+kubectl label node pico2 blinktShow=true --overwrite
+kubectl label node pico2 blinktImage=pods --overwrite
+kubectl label node pico2 blinktReadyColor=cpu --overwrite
 
-kubectl label node pico1 blinktImage=pods
-kubectl label node pico1 blinktShow=true
-kubectl label node pico1 blinktReadyColor=cpu
-sleep 1
+kubectl label node pico3 blinktShow=true --overwrite
+kubectl label node pico3 blinktImage=pods --overwrite
+kubectl label node pico3 blinktReadyColor=cpu --overwrite
 
-kubectl label node pico2 blinktImage=pods
-kubectl label node pico2 blinktShow=true
-kubectl label node pico2 blinktReadyColor=cpu
-sleep 1
-
-kubectl label node pico3 blinktImage=pods
-kubectl label node pico3 blinktShow=true
-kubectl label node pico3 blinktReadyColor=cpu
-sleep 1
-
-kubectl label node pico4 blinktImage=pods
-kubectl label node pico4 blinktShow=true
-kubectl label node pico4 blinktReadyColor=cpu
-sleep 1
+kubectl label node pico4 blinktShow=true --overwrite
+kubectl label node pico4 blinktImage=pods --overwrite
+kubectl label node pico4 blinktReadyColor=cpu --overwrite
